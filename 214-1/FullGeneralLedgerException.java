f70f4536700966a3e6e39cf93e5630081b9d367a
Cesare Lucido <clucidojr123@gmail.com>
2020-08-29 12:08:55 -0400

    remove info

--- 214-1/FullGeneralLedgerException.java
+++ 214-1/FullGeneralLedgerException.java
@@ -2,7 +2,6 @@
  * This exception is thrown when the ledger for a generalLedger object is full
  * @author Cesare Lucido
  *      e-mail: cesare.lucido@stonybrook.edu
- *      sbid: 112831455
  *      R01
  */
 public class FullGeneralLedgerException extends Exception{
